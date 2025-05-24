a6f697ed3ad11e924aabd14541e31058e0033bad
MetisWastaken <luvi05@icloud.com>
2025-05-24 14:12:52 +0200

    libreria completata, da aggiungersi modifiche secondarie

--- main.cpp
+++ main.cpp
@@ -10,8 +10,24 @@
 /* ************************************************************************** */
 
 int main() {
-  std::cout << "LASD Libraries 2025" << std::endl;
-lasdtest();
- //mytest();
+std::cout << "-----LASD Libraries 2025-----" << std::endl;
+int choice;
+std::cout << "Test choice:\n";
+std::cout << "1. lasdtest\n";
+std::cout << "2. mytest\n";
+std::cout << "Select test 1/2 or press any other key to exit: \n";
+std::cin >> choice;
+
+switch(choice) {
+  case 1:
+    lasdtest();
+    break;
+  case 2:
+    mytest();
+    break;
+  default:
+    std::cout << "Exiting now...Goodbye!" << std::endl;
+    break;
+}
   return 0;
 }
